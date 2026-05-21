9a92221498726e7812054789b4a2b9c67cad9784
Joshua <joshuanaulak2010@gmail.com>
2026-05-21 11:50:29 +0530

    Updated sw.js

--- sw.js
+++ sw.js
@@ -2,35 +2,64 @@
 
 const Cache_name = "website-cache-v1"; //name of the cache storage, can be anything
 
-const urlToCache = [ //they are files stored offline so that when user opens the app
+const StaticCache = [ //they are files stored offline so that when user opens the app
     "/Website/", // the browser download these and stores them locally
     "/Website/index.html", //so that the website can work offline
     "/Website/styling.css",
+    "/Website/manifest.json",
+    "/Website/icons/SUBWAY-192x192.png",
+    "/Website/icons/SUBWAY-512x512.png"
 ];
 
 
 //this works when Service Worker works for the first time
 self.addEventListener("install", e =>{
-    console.log("Service work is now installing");
-
     e.waitUntil(
-        caches.open(Cache_name) // opens browser storage
-        .then(cache => {
-            console.log("Catching files");
-            return cache.addAll(urlToCache); // download all the listing files into offline cache
+        caches.open(Cache_name)
+        .then((cache) => {
+            return cache.addAll(StaticCache);
         })
     );
+    self.skipWaiting();
 });
 
 
-//runs everytime the website request something
-//it can be html,images,js etc
+self.addEventListener("activate", e =>{
+    e.waitUntil(
+        caches.keys().then((keys) =>{
+            return Promise.all(
+                keys.map((key) => {
+                    if(key !== Cache_name){
+                        return caches.delete(key)
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+})
+
 self.addEventListener("fetch", e =>{
+    const request = e.request;
+
+    if(request.mode === "navigate"){
+        e.respondWith(
+            fetch(request).catch(() => caches.match("/Website/offline.html"))
+        );
+        return;
+    }
+
     e.respondWith(
-        caches.match(e.request)// checks if we have the files offline
-                               
-            .then(response =>{//if yes, then serve it
-            return response || fetch(e.request);//else, fetch it from the internet
+        caches.match(request).then((cached) =>{
+            return(
+                cached || fetch(request).then((response) =>{
+                    const copy = response.clone();
+                    caches.open(Cache_name).then((cache) =>{
+                        cache.put(request,copy);
+                    });
+                    return response;
+                })
+            );
         })
     );
 });
